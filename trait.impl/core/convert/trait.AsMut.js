2b30ca2aeb02a3d2cf1295812f940dd3c01e26dc
AriajSarkar <AriajSarkar@users.noreply.github.com>
2025-11-19 18:53:08 +0000

    deploy: ac9fe546c7740d100a79633819bcdf256eedbbdc

---
+++ trait.impl/core/convert/trait.AsMut.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["crabgraph",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.91.1/std/primitive.u8.html\">u8</a>]&gt; for <a class=\"struct\" href=\"crabgraph/secrets/struct.SecretVec.html\" title=\"struct crabgraph::secrets::SecretVec\">SecretVec</a>"],["impl&lt;const N: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.91.1/std/primitive.usize.html\">usize</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.91.1/std/primitive.u8.html\">u8</a>]&gt; for <a class=\"struct\" href=\"crabgraph/secrets/struct.SecretArray.html\" title=\"struct crabgraph::secrets::SecretArray\">SecretArray</a>&lt;N&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[914]}
